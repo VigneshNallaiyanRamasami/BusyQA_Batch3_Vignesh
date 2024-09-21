97d071070fa57c4fa40117793d786a8a9d04b6fb
VigneshNallaiyanRamasami <nrvignesh.nr@gmail.com>
2024-09-21 18:18:41 -0400

    Commiting to New_NR_Branch with new files

---
+++ src/com/learning/basicofJava/Test01.java
@@ -0,0 +1,29 @@
+package com.learning.basicofJava;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Learning Java");
+		System.out.println("Hello world");
+				
+				int age = 10; //4 bytes
+				byte kid = 126; //2 bytes
+				
+				System.out.println("The Age value in the integer variable is: "+age);
+				System.out.println("Kid value in the byte variable is: "+kid);
+				
+				short babies = 3;
+				long bigNumber = 787879789;
+				
+				float flotnum = 15.5f;
+				double doublenum = 20.2d;
+				
+//				String name = "Vignesh";
+//				
+//				char character = 'A';
+				
+			
+	}
+
+}
